95826e92155872bc884dd39d036c6d8ef817f5d0
Jorge Chávez Huincho <djjmygm160399@gmail.com>
2024-06-07 08:34:51 -0500

    producto terminado

--- vistas/modulos/header.php
+++ vistas/modulos/header.php
@@ -61,99 +61,58 @@
 
 
         <li class="nav-item dropdown">
+            <?php
+            
+            $item = null;
+            $valor = null;
+
+            $productosPorVencer = ControladorProducto::ctrMostrarProductosFechaVencimientos($item, $valor);
+
+            $totalProductosVencer = count($productosPorVencer);
+            ?>
             <a href="javascript:void(0);" class="dropdown-toggle nav-link" data-bs-toggle="dropdown">
-                <img src="vistas/dist/assets/img/icons/notification-bing.svg" alt="img"> <span class="badge rounded-pill">4</span>
+                <img src="vistas/dist/assets/img/icons/notification-bing.svg" alt="img"> <span class="badge rounded-pill" id="cantidad_notificacion"><?php echo $totalProductosVencer?></span>
             </a>
             <div class="dropdown-menu notifications">
                 <div class="topnav-dropdown-header">
                     <span class="notification-title">Notificaciones</span>
-                    <a href="javascript:void(0)" class="clear-noti">Limpiar todo</a>
+                    <a href="javascript:void(0)" class="clear-noti" id="clear-noti">Limpiar todo</a>
                 </div>
                 <div class="noti-content">
-                    <ul class="notification-list">
+                    <ul class="notification-list" id="notification-list">
+
+                        <?php
+
+                        foreach ($productosPorVencer as $producto){
+                        ?>
+
                         <li class="notification-message">
                             <a href="activities.html">
                                 <div class="media d-flex">
                                     <span class="avatar flex-shrink-0">
-                                        <img alt="" src="vistas/dist/assets/img/profiles/avatar-02.jpg">
+                                        <img alt="" src="<?php echo substr($producto["imagen_producto"],3)?>">
                                     </span>
                                     <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">John Doe</span> added
-                                            new task <span class="noti-title">Patient appointment booking</span>
+                                        <p class="noti-details"><span class="noti-title"><?php echo $producto["nombre_producto"]?></span> <?php echo $producto["nombre_categoria"]?>
+                        
                                         </p>
-                                        <p class="noti-time"><span class="notification-time">4 mins ago</span>
+                                        <p class="noti-time"><span class="notification-time"><?php echo $producto["fecha_vencimiento"]?></span>
                                         </p>
                                     </div>
                                 </div>
                             </a>
                         </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media d-flex">
-                                    <span class="avatar flex-shrink-0">
-                                        <img alt="" src="vistas/dist/assets/img/profiles/avatar-03.jpg">
-                                    </span>
-                                    <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">Tarah Shropshire</span>
-                                            changed the task name <span class="noti-title">Appointment booking
-                                                with payment gateway</span></p>
-                                        <p class="noti-time"><span class="notification-time">6 mins ago</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media d-flex">
-                                    <span class="avatar flex-shrink-0">
-                                        <img alt="" src="vistas/dist/assets/img/profiles/avatar-06.jpg">
-                                    </span>
-                                    <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">Misty Tison</span>
-                                            added <span class="noti-title">Domenic Houston</span> and <span class="noti-title">Claire Mapes</span> to project <span class="noti-title">Doctor available module</span></p>
-                                        <p class="noti-time"><span class="notification-time">8 mins ago</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media d-flex">
-                                    <span class="avatar flex-shrink-0">
-                                        <img alt="" src="vistas/dist/assets/img/profiles/avatar-17.jpg">
-                                    </span>
-                                    <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">Rolland Webber</span>
-                                            completed task <span class="noti-title">Patient and Doctor video
-                                                conferencing</span></p>
-                                        <p class="noti-time"><span class="notification-time">12 mins ago</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="notification-message">
-                            <a href="activities.html">
-                                <div class="media d-flex">
-                                    <span class="avatar flex-shrink-0">
-                                        <img alt="" src="vistas/dist/assets/img/profiles/avatar-13.jpg">
-                                    </span>
-                                    <div class="media-body flex-grow-1">
-                                        <p class="noti-details"><span class="noti-title">Bernardo Galaviz</span>
-                                            added new task <span class="noti-title">Private chat module</span>
-                                        </p>
-                                        <p class="noti-time"><span class="notification-time">2 days ago</span>
-                                        </p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
+
+
+                        <?php
+                        }
+                        ?>
+                        
+
                     </ul>
                 </div>
                 <div class="topnav-dropdown-footer">
-                    <a href="activities.html">View all Notifications</a>
+                    <a href="productos">Ver todas las notificaciones</a>
                 </div>
             </div>
         </li>
@@ -203,3 +162,10 @@
     </div>
 
 </div>
+
+<script>
+document.getElementById('clear-noti').addEventListener('click', function() {
+    document.getElementById('notification-list').innerHTML = '';
+    document.getElementById('cantidad_notificacion').innerHTML = '0';
+});
+</script>
